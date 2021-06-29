59792d2a6c16fc8a3155f393a965085c48731a3a
Faiz Alam <faizalam9883@protonmail.com>
2021-06-29 17:33:59 +0530

    3.14
    took 10 mins

--- index.js
+++ index.js
@@ -20,29 +20,6 @@ app.use(
 	)
 )
 
-let persons = [
-	{
-		id: 1,
-		name: 'Arto Hellas',
-		number: '040-123456',
-	},
-	{
-		id: 2,
-		name: 'Ada Lovelace',
-		number: '39-44-5323523',
-	},
-	{
-		id: 3,
-		name: 'Dan Abramov',
-		number: '12-43-234345',
-	},
-	{
-		id: 4,
-		name: 'Mary Poppendick',
-		number: '39-23-6423122',
-	},
-]
-
 app.get('/', (request, response) => {
 	response.send('Hello World')
 })
@@ -59,48 +36,42 @@ app.get('/api/persons/:id', (request, response) => {
 	})
 })
 
-app.get('/info', (request, response) => {
-	const info = `<p>
-				Phonebook has info for ${persons.length} people
-				</p>
-				<p>${Date()}</p>`
-	response.send(info)
-})
+// app.get('/info', (request, response) => {
+// 	const info = `<p>
+// 				Phonebook has info for ${persons.length} people
+// 				</p>
+// 				<p>${Date()}</p>`
+// 	response.send(info)
+// })
 
 app.post('/api/persons', (request, response) => {
 	const body = request.body
-
-	const id = parseInt(Math.random() * 1000)
 
 	if (!body.name || !body.number) {
 		return response.status(400).json({
 			error: 'name or number missing',
 		})
 	}
-
-	if (persons.find((p) => p.name === body.name)) {
-		return response.status(400).json({
-			error: 'name must be unique',
-		})
-	}
-
-	const person = {
-		id: id,
+	// if (persons.find((p) => p.name === body.name)) {
+	// 	return response.status(400).json({
+	// 		error: 'name must be unique',
+	// 	})
+	// }
+	const person = new Person({
 		name: body.name,
 		number: body.number,
-	}
-
-	persons = persons.concat(person)
-
-	response.json(person)
+	})
+	person.save().then((savedPerson) => {
+		response.json(savedPerson)
+	})
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-	const id = Number(request.params.id)
-	persons = persons.filter((p) => p.id !== id)
+// app.delete('/api/persons/:id', (request, response) => {
+// 	const id = Number(request.params.id)
+// 	persons = persons.filter((p) => p.id !== id)
 
-	response.status(204).end()
-})
+// 	response.status(204).end()
+// })
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
